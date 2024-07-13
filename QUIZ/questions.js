74100c0ad448bc5e76229104f92411cf28584fa9
shun0224006200726 <shun.085@gmai.com>
2024-07-13 19:51:26 +0900

    2040713-3

--- QUIZ/questions.js
+++ QUIZ/questions.js
@@ -1,17 +1,19 @@
 const answersList = document.querySelectorAll('ol.answers li');
 
 answersList.forEach(li => li.addEventListener('click',checkClickedAnswer));
-function checkClickedAnswer(){
+function checkClickedAnswer(event){
   
+  let clickedAnswerElement=event.currentTarget;
+  let selectedAnswer="A";
+  let correctedAnswer=clickedAnswerElement.dataset.answer;
 
-  
   let message;
   let answerColorCode;
 
 
   
   
-  if(){
+  if(correctedAnswer==selectedAnswer){
     message='正解';
     answerColorCode ='';
   }else{
